e04d6ca4d4679b2f686b3f09d821945a77158779
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-13 16:42:43 -0700

    bubble sort fun times

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,11 +7,9 @@ class Heap:
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        if self.storage[0]:
+        if len(self.storage):
             return_val = self.storage[0]
             self.storage[0] = self.storage[len(self.storage) - 1]
-            # temp = self.storage.pop()
-            # self.storage[0] = temp
             self.storage.pop()
             self._sift_down(0)
         return return_val
@@ -23,23 +21,21 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        if self.storage[(index - 1) // 2]:
-            if self.storage[(index - 1) // 2] < self.storage[index]:
-                temp = self.storage[index]
-                self.storage[index] = self.storage[(index - 1) // 2]
-                self.storage[(index - 1) // 2] = temp
-                if self.storage[(((index - 1) // 2) - 1) // 2] and self.storage[(((index - 1) // 2) - 1) // 2] < self.storage[(index - 1) // 2]:
-                    self._bubble_up((index - 1) // 2)
-                else:
-                    self._sift_down(index // 2)
+        # if self.storage[index // 2]:
+        if self.storage[index // 2] < self.storage[index]:
+            temp = self.storage[index]
+            self.storage[index] = self.storage[index // 2]
+            self.storage[index // 2] = temp
+            if ((index // 2) // 2) >= 0:
+                if self.storage[(index // 2) // 2] < self.storage[index // 2]:
+                    self._bubble_up(index // 2)
+            else:
+                self._sift_down(index)
 
     def _sift_down(self, index):
-        print(index)
-        print((index * 2) + 1)
-        print((index * 2) + 2)
-        temp = self.storage[index]
-        if self.storage[(index * 2) + 1]:
-            if self.storage[(index * 2) + 2]:
+        if ((index * 2) + 1) < len(self.storage):
+            temp = self.storage[index]
+            if ((index * 2) + 2) < len(self.storage):
                 if self.storage[(index * 2) + 1] > self.storage[(index * 2) + 2]:
                     if self.storage[(index * 2) + 1] > self.storage[index]:
                         self.storage[index] = self.storage[(index * 2) + 1]
