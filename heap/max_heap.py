cad1f8e4039f8fabe895b8c7ac6ec7feb6885151
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-13 15:39:17 -0700

    fixes in progress

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,9 +7,14 @@ class Heap:
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        temp = self.storage.pop()
-        self.storage[0] = temp
-        self._sift_down(0)
+        if self.storage[0]:
+            return_val = self.storage[0]
+            self.storage[0] = self.storage[len(self.storage) - 1]
+            # temp = self.storage.pop()
+            # self.storage[0] = temp
+            self.storage.pop()
+            self._sift_down(0)
+        return return_val
 
     def get_max(self):
         return self.storage[0]
@@ -18,13 +23,13 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        if self.storage[index // 2]:
-            if self.storage[index // 2] < self.storage[index]:
+        if self.storage[(index - 1) // 2]:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
                 temp = self.storage[index]
-                self.storage[index] = self.storage[index // 2]
-                self.storage[index // 2] = temp
-                if self.storage[(index // 2) // 2] and self.storage[(index // 2) // 2] < self.storage[index // 2]:
-                    self._bubble_up(index // 2)
+                self.storage[index] = self.storage[(index - 1) // 2]
+                self.storage[(index - 1) // 2] = temp
+                if self.storage[(((index - 1) // 2) - 1) // 2] and self.storage[(((index - 1) // 2) - 1) // 2] < self.storage[(index - 1) // 2]:
+                    self._bubble_up((index - 1) // 2)
                 else:
                     self._sift_down(index // 2)
 
